145f3d4df53f17cdf565b37236f2582e6954cec5
Michael Ulianich <ulianich_mihail@ukr.net>
2024-05-29 22:43:27 +0300

    Refactor: use flask application factory

--- app/__init__.py
+++ app/__init__.py
@@ -6,17 +6,39 @@ from .database import db
 from .auth.models import User, Role
 
 
-ticket_system = flask.Flask(__name__)
-ticket_system.config.from_prefixed_env('FLASK')
+def create_app():
+    ticket_system = flask.Flask(__name__)
+    ticket_system.config.from_prefixed_env('FLASK')
+    if ticket_system.debug:
+        from pprint import pprint as print
+        print(dict(ticket_system.config))
 
-if ticket_system.debug:
-    from pprint import pprint as print
-    print(dict(ticket_system.config))
+    db.init_app(ticket_system)
 
-db.init_app(ticket_system)
+    ticket_system.security = flask_security.Security(
+        ticket_system,
+        flask_security.SQLAlchemyUserDatastore(db, User, Role),
+    )
 
-ticket_system.security = flask_security.Security(
-    ticket_system,
-    flask_security.SQLAlchemyUserDatastore(db, User, Role),
-)
+    # TODO: set up a migration tool, Alembic
+    @ticket_system.cli.command('db_create_all')
+    def db_create_all():
+        """Populate an empty database"""
+        import app.auth.models
+        import app.tickets.models
+
+        with ticket_system.app_context():
+            db.create_all()
+            for i in range(1, 4):
+                db.session.add(
+                    app.tickets.models.Group(name=f'Customer {i}'),
+                )
+            db.session.commit()
+
+    import app.tickets.views
+    import app.auth.views
+    ticket_system.register_blueprint(app.tickets.views.blueprint)
+    ticket_system.register_blueprint(app.auth.views.blueprint)
+
+    return ticket_system
 
